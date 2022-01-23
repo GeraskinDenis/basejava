698716a7d6ab553407874c583d0c14e43cc6f666
Geraskin Denis <fresh7tea@gmail.com>
2022-01-23 04:36:11 +0300

    class ArrayStorage implemented save()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
+public class ArrayStorage
+{
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    void clear()
+    {
     }
 
-    void save(Resume r) {
+    void save(Resume resume)
+    {
+        if (resume == null) return;
+
+        if (resume.uuid.isEmpty()) return;
+
+        for (int i = 0; i < storage.length; i++)
+        {
+            if (storage[i].uuid.equals(resume.uuid))
+            {
+                // Перезаписывать или выбрасить Exception???
+                storage[i] = resume;
+                return;
+            }
+        }
+
+        if ((size + 1) > storage.length)
+        {
+            // Что делать при переполнении базы???
+            return;
+        }
+
+        storage[size++] = resume;
     }
 
-    Resume get(String uuid) {
+    Resume get(String uuid)
+    {
         return null;
     }
 
-    void delete(String uuid) {
+    void delete(String uuid)
+    {
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    Resume[] getAll()
+    {
         return new Resume[0];
     }
 
-    int size() {
+    int size()
+    {
         return 0;
     }
 }
