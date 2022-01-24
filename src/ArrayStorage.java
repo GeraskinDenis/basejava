749701cfd2ab6e1cf3f62244b43e54293bc66c8d
Geraskin Denis <fresh7tea@gmail.com>
2022-01-24 13:23:51 +0300

    class ArrayStorage refactoring

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,102 +3,50 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage
-{
+public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear()
-    {
-        for (int i = 0; i < size - 1; i++)
-        {
+    void clear() {
+        for (int i = 0; i < size - 1; i++) {
             storage[i] = null;
         }
-
         size = 0;
     }
 
-    void save(Resume resume)
-    {
-        if (resume == null) return;
-
-        if (resume.uuid.isEmpty()) return;
-
-        for (int i = 0; i < size; i++)
-        {
-            if (storage[i].uuid.equals(resume.uuid))
-            {
-                // Перезаписывать или выбрасить Exception???
-                storage[i] = resume;
-                return;
-            }
-        }
-
-        if ((size + 1) > storage.length)
-        {
-            // Что делать при переполнении размера базы???
-            return;
-        }
-
+    void save(Resume resume) {
         storage[size++] = resume;
     }
 
-    Resume get(String uuid)
-    {
-        for (int i = 0; i < size; i++)
-        {
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid))
                 return storage[i];
         }
-
         return null;
-
     }
 
-    void delete(String uuid)
-    {
-        if (uuid.isEmpty())
-        {
-            return;
-        }
-
-        int indexOfNull = -1;
-
-        for (int i = 0; i < size; i++)
-        {
-            if (storage[i].uuid.equals(uuid))
-            {
-                storage[i] = null;
-                indexOfNull = i;
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                if (i == (size - 1)) {
+                    size--;
+                } else {
+                    storage[i] = storage[size-- - 1];
+                }
                 break;
             }
         }
-
-        if (indexOfNull == -1)
-        {
-            return;
-        }
-
-        if (indexOfNull != (size - 1))
-        {
-            storage[indexOfNull] = storage[size - 1];
-            storage[size - 1] = null;
-        }
-
-        size--;
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll()
-    {
+    Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size()
-    {
+    int size() {
         return size;
     }
 }
