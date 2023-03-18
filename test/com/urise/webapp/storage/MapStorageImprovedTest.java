a23f3b403d560f8c566cde3e3a3f7cd1040221ae
Geraskin Denis <fresh7tea@gmail.com>
2023-03-18 11:29:06 +0300

    HW6: Add MapStorageImprovedTest.java

---
+++ test/com/urise/webapp/storage/MapStorageImprovedTest.java
@@ -0,0 +1,8 @@
+package com.urise.webapp.storage;
+
+public class MapStorageImprovedTest extends AbstractStorageTest<MapStorageImproved> {
+
+    public MapStorageImprovedTest() {
+        super(new MapStorageImproved());
+    }
+}
